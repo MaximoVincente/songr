69d0d49dc728371c4a0123556bd63eeaa264acee
Maximo Vincente Mejia <maximovincente@gmail.com>
2022-11-07 16:49:39 -0500

    initial commit from Spring init

---
+++ src/test/java/com/vincenteSongr/songr/SongrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincenteSongr.songr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
