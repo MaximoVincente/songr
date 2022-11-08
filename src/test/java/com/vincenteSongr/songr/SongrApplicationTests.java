dd95fa800bdf8f8af5af56396a15442aa202c5ce
Maximo Vincente Mejia <maximovincente@gmail.com>
2022-11-08 00:13:36 -0500

    Added Tests

--- src/test/java/com/vincenteSongr/songr/SongrApplicationTests.java
+++ src/test/java/com/vincenteSongr/songr/SongrApplicationTests.java
@@ -1,7 +1,11 @@
 package com.vincenteSongr.songr;
 
+import com.vincenteSongr.songr.model.Album;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SongrApplicationTests {
@@ -10,4 +14,15 @@ class SongrApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void testToString() {
+		Album sut = new Album("The Razors Edge", "AC/DC", 12, 2802, "imgURL");
+		assertEquals(sut.toString(), "Album The Razors Edge from AC/DC and its 12 hit songs!");
+	}
+
+	@Test
+	void testAddAlbum(){
+		Album sut = new Album("The Razors Edge", "AC/DC", 12, 2802, "imgURL");
+		assertNotNull(sut);
+	}
 }
